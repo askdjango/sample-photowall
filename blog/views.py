155b6af8511833c17262accde61b30770ab6e7f3
Chinseok Lee <allieuslee@gmail.com>
2017-04-19 03:10:32 +0900

    blog.views.index 뷰 구현

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def index(request):
+    post_list = Post.objects.all()
+    return render(request, 'blog/post_list.html', {
+        'post_list': post_list,
+    })
