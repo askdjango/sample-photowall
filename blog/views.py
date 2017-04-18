6f45fd2a77b7be9ad81669bf204a85796870fc6e
Chinseok Lee <allieuslee@gmail.com>
2017-04-19 03:17:57 +0900

    blog.views.post_detail 뷰 구현

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Post
 
 
@@ -6,4 +6,11 @@ def index(request):
     post_list = Post.objects.all()
     return render(request, 'blog/post_list.html', {
         'post_list': post_list,
+    })
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {
+        'post': post,
     })
