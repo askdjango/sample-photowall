1475dbecbecac6d95be43c00a7a7c9c0950950f7
Chinseok Lee <allieuslee@gmail.com>
2017-04-23 18:20:21 +0900

    blog.models.Post 모델에 lnglat 필드를 추가하고, 입력된 경도/위도에 맞춰 구글맵 노출

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,33 @@
+import re
+from django import forms
 from django.db import models
+
+
+def lnglat_validator(value):
+    if not re.match(r'^[+-]?\d+\.?\d*,[+-]?\d+\.?\d*$', value):
+        raise forms.ValidationError('경도/위도를 입력해주세요.')
 
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     photo = models.ImageField()
+    lnglat = models.CharField(max_length=40, blank=True, validators=[lnglat_validator])
     is_public = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+    
+    @property
+    def lng(self):
+        if self.lnglat:
+            return self.lnglat.split(',')[0]
+        return None
+    
+    @property
+    def lat(self):
+        if self.lnglat:
+            return self.lnglat.split(',')[1]
+        return None
